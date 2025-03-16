09a83e77b774ab20d4e076c13637c2896726de67
severocris332 <severocris332@gmail.com>
2025-03-16 10:26:14 +0000

    Add 10.ts

---
+++ src/10.ts
@@ -0,0 +1,73 @@
+export default {
+  title: 'Math Problems',
+  problemSets: [
+    {
+      name: 'Addition',
+      problems: [
+        {
+          title: '1 + 2 = ?',
+          answer: 3,
+        },
+        {
+          title: '5 + 7 = ?',
+          answer: 12,
+        },
+        {
+          title: '8 + 4 = ?',
+          answer: 12,
+        },
+      ],
+    },
+    {
+      name: 'Subtraction',
+      problems: [
+        {
+          title: '7 - 3 = ?',
+          answer: 4,
+        },
+        {
+          title: '9 - 5 = ?',
+          answer: 4,
+        },
+        {
+          title: '12 - 8 = ?',
+          answer: 4,
+        },
+      ],
+    },
+    {
+      name: 'Multiplication',
+      problems: [
+        {
+          title: '3 x 5 = ?',
+          answer: 15,
+        },
+        {
+          title: '7 x 4 = ?',
+          answer: 28,
+        },
+        {
+          title: '9 x 6 = ?',
+          answer: 54,
+        },
+      ],
+    },
+    {
+      name: 'Division',
+      problems: [
+        {
+          title: '18 / 3 = ?',
+          answer: 6,
+        },
+        {
+          title: '20 / 4 = ?',
+          answer: 5,
+        },
+        {
+          title: '27 / 9 = ?',
+          answer: 3,
+        },
+      ],
+    },
+  ],
+};
