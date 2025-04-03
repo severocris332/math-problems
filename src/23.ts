d2721c96e482c6ff2d51229692d72dd368db7656
severocris332 <severocris332@gmail.com>
2025-04-03 12:51:33 +0000

    Add 23.ts

---
+++ src/23.ts
@@ -0,0 +1,9 @@
+import numpy as np
+from scipy.optimize import fsolve
+
+def quadratic_function(x):
+    return x**2 - 3*x + 4
+
+initial_guess = [1, 2]
+roots = fsolve(quadratic_function, initial_guess)
+print(roots)
