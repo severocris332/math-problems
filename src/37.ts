80a5b749805b3392f5ccdbe2f3d5f61494a52789
severocris332 <severocris332@gmail.com>
2025-05-20 04:48:26 +0000

    Add 37.ts

---
+++ src/37.ts
@@ -0,0 +1,11 @@
+import numpy as np
+from sympy import symbols
+
+# Define symbolic variables
+x = symbols('x')
+
+# Your math problem here...
+solution = x ** 2 - 3 * x + 4
+
+# Print the solution
+print(solution)
