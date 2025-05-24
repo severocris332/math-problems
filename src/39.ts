484ce9a19c651839765d28d0258fe9937171b3c7
severocris332 <severocris332@gmail.com>
2025-05-24 20:39:41 +0000

    Add 39.ts

---
+++ src/39.ts
@@ -0,0 +1,8 @@
+import numpy as np
+
+def calculate_expression(expression):
+    result = eval(expression)
+    return result
+
+expression = "3 + 5 * (2 - 1)"
+print(calculate_expression(expression))
